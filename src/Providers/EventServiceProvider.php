018cc8f9ed15b79ed4ba9d9dcddb0b875e95463a
Lance Pioch <git@lance.sh>
2019-04-03 22:09:21 -0400

    Not used

--- src/Providers/EventServiceProvider.php
+++ src/Providers/EventServiceProvider.php
@@ -4,7 +4,6 @@ namespace Scoutapm\Laravel\Providers;
 
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Routing\Events\RouteMatched;
-use Scoutapm\Laravel\Events\Listener;
 use Scoutapm\Laravel\Events\RouteMatchedEvent;
 use Scoutapm\Laravel\Events\TemplateCreatedEvent;
 use Scoutapm\Laravel\Events\TemplateRenderedEvent;
