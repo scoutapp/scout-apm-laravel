33b2eb122325fb2729010b0476a234c5a1a8531a
Chris Schneider <chris@scoutapp.com>
2019-07-15 14:56:59 -0600

    Remove unneded logging in SendRequestToScout

--- src/Middleware/SendRequestToScout.php
+++ src/Middleware/SendRequestToScout.php
@@ -14,21 +14,17 @@ class SendRequestToScout
     public function __construct(Agent $agent)
     {
         $this->agent = $agent;
-        $this->agent->getLogger()->info("SendRequestToScout initialized");
     }
 
     public function handle($request, Closure $next)
     {
-        $this->agent->getLogger()->info("SendRequestToScout handle");
         $response = $next($request);
-        $this->agent->getLogger()->info("SendRequestToScout After handle");
         return $response;
     }
 
     public function terminate($request, $response)
     {
         try {
-            $this->agent->getLogger()->info("SendRequestToScout terminate");
             $this->agent->send();
         } catch (\Exception $e) {
             $this->agent->getLogger()->info("SendRequestToScout failed: ");
